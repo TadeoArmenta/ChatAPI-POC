5d91e8ba600edd2b34384a3889846b7124d6bbf2
TadeoArmenta <cgtarmenta@gmail.com>
2021-09-20 00:19:55 +0200

    Update deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,29 @@
-{
-
-}
 module.exports = {
     apps: [
         {
-            "name":               "VAYYUP:DEVICES:API",
-            "script":             "server.js",
-            "ignore_watch":       ["node_modules","logs"],
-            "exec_mode":          "cluster_mode",
-            "instances":          2,
-            "watch":              true,
-            "merge_logs":         true,
-            "env": {
-                "NODE_ENV":         "production",
-                "UV_THREADPOOL_SIZE":"10"
+            name            : 'VAYYUP:DEVICES:API',
+            script          : 'server.js',
+            ignore_watch    : ['node_modules','logs'],
+            exec_mode       : 'cluster_mode',
+            instances       : 2,
+            watch           : true,
+            merge_logs      : true,
+            env             : {
+                'NODE_ENV'              : 'production',
+                'UV_THREADPOOL_SIZE'    : '10'
             }
         }
     ],
     deploy: {
         production: {
-            key: '~/.ssh/id_rsa',
-            user: 'ubuntu',
-            host: 'vayyup.tadeoarmenta.com',
-            ref: 'origin/master',
-            repo: 'git@github.com:TadeoArmenta/Vayyup.git',
-            path: '/home/ubuntu/Vayyup',
-            /// Source the user's .zshrc file first!!
-            'post-deploy': `source ~/.bashrc && \
-             yarn install --ignore-engines && \
+            key         : '~/.ssh/id_rsa',
+            user        : 'ubuntu',
+            host        : 'vayyup.tadeoarmenta.com',
+            ref         : 'origin/master',
+            repo        : 'git@github.com:TadeoArmenta/Vayyup.git',
+            path        : '/home/ubuntu/Vayyup',
+            'post-deploy': `source ~/.profile && \
+             yarn && \
              pm2 reload ecosystem.config.js --env production && \
              pm2 save && \
              sudo cp nginx-deploy.conf /etc/nginx/sites-available/vayyup.conf`
