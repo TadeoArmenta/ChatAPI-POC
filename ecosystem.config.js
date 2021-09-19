9bccb7ecbabe3f100620b103a57b6680618fe65d
TadeoArmenta <cgtarmenta@gmail.com>
2021-09-19 23:23:04 +0200

    Update deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,8 @@
 {
-    "apps" : [
+
+}
+module.exports = {
+    apps: [
         {
             "name":               "VAYYUP:DEVICES:API",
             "script":             "server.js",
@@ -13,5 +16,15 @@
                 "UV_THREADPOOL_SIZE":"10"
             }
         }
-    ]
-}
+    ],
+    deploy: {
+        production: {
+            host: 'tadeoarmenta.com',
+            ref: 'origin/master',
+            repo: "git@github.com:TadeoArmenta/Vayyup.git", // your repo url
+            path: "/home/ubuntu/Vayyup",
+            "post-deploy":
+                "npm install && pm2 reload ecosystem.config.js --env production && pm2 save"
+        }
+    }
+};
