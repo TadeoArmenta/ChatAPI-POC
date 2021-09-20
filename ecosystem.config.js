edc3b7c7527b375278b679813b2b9af2639c62f0
TadeoArmenta <cgtarmenta@gmail.com>
2021-09-20 10:27:11 +0200

    Update deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,11 +22,16 @@ module.exports = {
             ref         : 'origin/master',
             repo        : 'git@github.com:TadeoArmenta/Vayyup.git',
             path        : '/home/ubuntu/Vayyup',
+            'pre-deploy': `export NVM_DIR="$HOME/.nvm" \
+             [ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh"
+             [ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion" `,
             'post-deploy': `source ~/.profile && \
              yarn && \
              pm2 reload ecosystem.config.js --env production && \
              pm2 save && \
-             sudo cp nginx-deploy.conf /etc/nginx/sites-available/vayyup.conf`
+             sudo cp nginx-deploy.conf /etc/nginx/sites-available/vayyup.conf \
+             sudo ln -s /etc/nginx/sites-available/vayyup.conf /etc/nginx/sites-enable/vayyup.tadeoarmenta.com \
+             sudo nginx restart`
         }
     }
 };
